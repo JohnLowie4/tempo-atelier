99d217d308989eba7fa01588b9ca8fc1f119c286
john lowie <jzlowie@gmail.com>
2026-07-18 12:19:34 -0600

    set app to SPA

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,8 +2,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import HomeScreen from '@/app/index';
 
 import '@/global.css';
 
@@ -13,8 +12,7 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
+      <HomeScreen />
     </ThemeProvider>
   );
 }
